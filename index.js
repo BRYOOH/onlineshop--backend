618d4a5674b3fae3578432ee2636f2482fdb3009
BRYOOH <bryoohmuchira@gmail.com>
2024-07-17 19:46:33 +0300

    addproduct to db

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get("/",(req,res)=>{
     res.send("Express app is running");
 })
 
-//Image Stoarage Engine
+//Image Storage Engine
 const storage = multer.diskStorage({
     destination: './upload/images',
     filename: (req,file,cb)=>{
@@ -39,6 +39,61 @@ app.post("/upload",upload.single('product'),(req,res)=>{
     })
 })
 
+//Schema for products
+const Product=mongoose.model("Product",{
+    id:{
+        type:Number,
+        required:true,
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    new_price:{
+        type:Number,
+        required:true,
+    },
+    old_price:{
+        type:Number,
+        required:true,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    },
+    available:{
+        type: Boolean,
+        default:true,
+    },
+})
+
+app.post('/addproduct', async(req,res)=>{
+    const product =new Product({
+        id:req.body.id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+    });
+    
+    console.log(product);
+    await product.save();
+    console.log("Saved");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+
+})
 app.listen(port,(error)=>{
     if(!error){
         console.log("Server running on port: " +port);
