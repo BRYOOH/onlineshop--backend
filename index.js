a552311cc82c4d81b9fe1339ac3a3c82e4c3febe
BRYOOH <bryoohmuchira@gmail.com>
2024-08-13 21:11:53 +0300

    login API

--- index.js
+++ index.js
@@ -176,6 +176,29 @@ app.post('/signup', async(req,res)=>{
     res.json({success:true,token});
 })
 
+//API for login page
+app.post('/login',async(req, res)=>{
+let user = await Users.findOne({email:req.body.email});
+    if(user){
+        const checkpass = req.body.password === user.password;
+        if(checkpass){
+             const data ={
+                user:{
+                    id:user.id
+                }
+             }
+             const token = jwt.sign(data,'secret_ecom');
+             res.json({success:true,token});
+        }
+        else{
+            res.json({success:false, errors:"Wrong password"})
+        }
+    }
+    else{
+        res.json({success:false, errors:"Wrong user email"})
+    }
+})
+
 app.listen(port,(error)=>{
     if(!error){
         console.log("Server running on port: " +port);
